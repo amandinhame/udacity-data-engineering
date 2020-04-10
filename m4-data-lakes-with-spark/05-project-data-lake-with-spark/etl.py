ff2e4230e70b36175bf389cfdaa7bd6ef85757b4
Amandinha <amandinhame@users.noreply.github.com>
2020-04-10 12:22:53 +0200

    Changed the final S3 bucket variable

--- m4-data-lakes-with-spark/05-project-data-lake-with-spark/etl.py
+++ m4-data-lakes-with-spark/05-project-data-lake-with-spark/etl.py
@@ -142,7 +142,7 @@ def main():
     
     spark = create_spark_session()
     input_data = "s3a://udacity-dend/"
-    output_data = "s3a://motigome-dl/"
+    output_data = "s3a://[your-final-S3-bucket]/"
     
     process_song_data(spark, input_data, output_data)    
     process_log_data(spark, input_data, output_data)
